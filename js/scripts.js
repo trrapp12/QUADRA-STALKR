da5fc4c45295604f547367428e815a933b96a095
Trevor Rapp <trrapp@gmail.com>
2021-12-03 00:47:41 -0700

    fixes a ton of stuff from getting the parallel photo columns to jive, to creating a working call to action, to fixing body offset, to calculating the vector and spin of a neutrino after 3 expressos and a trip round the Hadron collider

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+(() => {
+
+  const missionSection = document.getElementById('mission-section');
+
+  let isVisible = function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+// function found in javascripttutorial.net
+window.addEventListener('scroll', function() {
+  if (isVisible(missionSection)) {
+    alert("is visible")
+  }
+});
+})();
