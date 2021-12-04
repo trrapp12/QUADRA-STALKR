d2cdb1602147bc435ffe14b485e4c568110277f4
Trevor Rapp <trrapp@gmail.com>
2021-12-03 20:04:28 -0700

    fixes the ghosting borders that doubled up when you hovered over the nav links and the really long containers by getting rid of CSS hover and applying the CSS through JS mouseenter and mouseout events

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,29 @@
 (() => {
-  let navLinks = document.querySelectorAll('.nav-link')
+  let navLinks = document.querySelectorAll('.nav-link');
 
   document.addEventListener('scroll', function () {
     for (let i = 0; i < navLinks.length; i++) {
-      navLinks[i].classList.add('nav-hover-javascript')
+      navLinks[i].classList.add('nav-hover-javascript');
     }
     function removeClass() {
       for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.remove('nav-hover-javascript')
+        navLinks[i].classList.remove('nav-hover-javascript');
       }
     }
     setTimeout(removeClass, 8000);
-  })
+  });
+
+  function addNodeListeners (navLinks) {
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('mouseenter', function () {
+        navLinks[i].classList.add('nav-link-2-hover');
+      });
+      navLinks[i].addEventListener('mouseout', function () {
+        navLinks[i].classList.remove('nav-link-2-hover');
+      });
+    };
+  }
+
+  addNodeListeners(navLinks);
 
 })();
