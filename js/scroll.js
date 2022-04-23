8e0cb63bcb11ea48b9c2a04790b3d0b49f4f9191
Trevor Rapp <trrapp@gmail.com>
2022-04-22 22:05:52 -0600

    fixed the JS and the CSS so they both resemeble the same action on a hover or a scroll, fixed the transition so it is smooth, fixed the issue where the nav got bigger when you hovered over it and the layout shifted

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 (() => {
-  let navLinks = document.querySelectorAll('.nav-link');
+  let navLinks = document.querySelectorAll('.nav-link-2');
 
   document.addEventListener('scroll', function () {
     for (let i = 0; i < navLinks.length; i++) {
@@ -10,19 +10,19 @@
         navLinks[i].classList.remove('nav-hover-javascript');
       }
     }
-    setTimeout(removeClass, 8000);
+    setTimeout(removeClass, 2500);
   });
 
-  function addNodeListeners (navLinks) {
-    for (let i = 0; i < navLinks.length; i++) {
-      navLinks[i].addEventListener('mouseenter', function () {
-        navLinks[i].classList.add('nav-link-2-hover');
-      });
-      navLinks[i].addEventListener('mouseout', function () {
-        navLinks[i].classList.remove('nav-link-2-hover');
-      });
-    };
-  }
+  // function addNodeListeners (navLinks) {
+  //   for (let i = 0; i < navLinks.length; i++) {
+  //     navLinks[i].addEventListener('mouseenter', function () {
+  //       navLinks[i].classList.add('nav-link-2-hover');
+  //     });
+  //     navLinks[i].addEventListener('mouseout', function () {
+  //       navLinks[i].classList.remove('nav-link-2-hover');
+  //     });
+  //   };
+  // }
 
   addNodeListeners(navLinks);
 
