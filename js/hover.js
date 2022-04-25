a53a7a953d4533e84e57b27a3136fb35932ae770
Trevor Rapp <trrapp@gmail.com>
2022-04-25 17:25:37 -0600

    cleans up the html that was sloppily organized. Adds more semantic tags

--- js/hover.js
+++ js/hover.js
@@ -3,7 +3,7 @@
     let titleH1 = document.getElementById('mission_section_header');
     let titleH1Two = document.getElementById('products_section_header');
     let titleContainer = document.getElementById('mission-section-container-id'); 
-    let titleContainer2 = document.getElementById('products-section-container-id')
+    let titleContainer2 = document.getElementById('catalog-section-container-id')
 
     titleContainer.addEventListener('mouseover', () => {
       titleH1.classList.add('mission-section-container-hover')
