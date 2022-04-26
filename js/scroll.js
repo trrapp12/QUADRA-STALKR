262d0964507c4d9a0435105e6e2e9f608ea226a0
Trevor Rapp <trrapp@gmail.com>
2022-04-26 00:32:48 -0600

    fixes spacing and hover effects

--- js/scroll.js
+++ js/scroll.js
@@ -10,19 +10,19 @@
         navLinks[i].classList.remove('nav-hover-javascript');
       }
     }
-    setTimeout(removeClass, 2500);
+    setTimeout(removeClass, 4500);
   });
 
-  // function addNodeListeners (navLinks) {
-  //   for (let i = 0; i < navLinks.length; i++) {
-  //     navLinks[i].addEventListener('mouseenter', function () {
-  //       navLinks[i].classList.add('nav-link-2-hover');
-  //     });
-  //     navLinks[i].addEventListener('mouseout', function () {
-  //       navLinks[i].classList.remove('nav-link-2-hover');
-  //     });
-  //   };
-  // }
+  function addNodeListeners (navLinks) {
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('mouseenter', function () {
+        navLinks[i].classList.add('nav-hover-javascript');
+      });
+      navLinks[i].addEventListener('mouseout', function () {
+        navLinks[i].classList.remove('nav-hover-javascript');
+      });
+    };
+  }
 
   addNodeListeners(navLinks);
 
