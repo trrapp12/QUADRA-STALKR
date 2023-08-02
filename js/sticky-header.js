4617e7874266f0b8f9d9a7d60fe6511933ac00ff
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-08-01 23:46:02 -0600

    adds beginning of CSS for mobile and a utility that detects the scroll height of the window and changes the background color of the nav when it scrolls down far enough.  :fire: :fire: :fire: Super cool use of a closure used to throttle the scroll listener :fire: :fire: :fire:

---
+++ js/sticky-header.js
@@ -0,0 +1,73 @@
+(() => {
+    // set constants
+    const elementHeight = '590px';
+    const scrollableElement = window;
+    const elementToChange = document.querySelectorAll('.nav-link-2')
+    const elementColor = 'black';
+
+    // check to see if object is window object
+    function isElementWindow(element) {
+        console.log(`isElementWindow() fired. Element is ${element}, logical comparison returns ${element === window}`)
+        return element === window;
+    }
+    // define scroll event handler
+    function scrollHandler(height, scrollableEl, element, color) {
+        console.log('scrollHandler fired')
+        console.log('element in scrollHander is ',element)
+        console.log(element)
+        // when the top of the window is equal to height of the header, turn header background opaque
+        for (const item in element) {
+            if (isElementWindow(scrollableEl)) {
+                if (scrollableEl.scrollY >= parseInt(height)) {
+                    console.log(`scrollableEl height is ${scrollableEl.scrollY}`)
+                    element[item].style.backgroundColor = color;
+                }
+                else {
+                    element[item].style.backgroundColor = 'transparent';
+                    console.log('object is not the window')
+                }
+            }
+        }
+    }
+
+    // create throttle handler
+    function throttleHandler(func, delay) {
+        let lastCall = 0;
+        return function(...args) {
+            let currentTime = Date.now();
+            if (lastCall - currentTime >= delay) {
+                func(...args);
+                lastCall = currentTime;
+            } 
+        }
+    }
+
+    // call scroll eventhandler in throttle handler
+    window.addEventListener('scroll', () => {
+        let tableData = [
+            {
+                name: "elementHeight",
+                value: elementHeight
+            },
+            {
+                name: "scrollableElement",
+                value: scrollableElement
+            },
+            {
+                name: "elementToChange",
+                value: elementToChange
+            },
+            {
+                name: "elementColor",
+                value: elementColor
+            }
+        ];
+
+        console.table(tableData)
+
+        throttleHandler(scrollHandler(elementHeight, scrollableElement, elementToChange, elementColor))
+    })
+    // attach handler to window.scroll event
+
+    
+})();
